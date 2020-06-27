9720e9d4b594933eab376c767918258bb9c45c37
siddy-1998 <siddhantrathore21@gmail.com>
2020-06-27 12:25:53 +0530

    add home and user_profile view

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,4 +1,7 @@
 module.exports.home = function(req,res)
 {
-    return res.end('<h1>Express is up and running for codial!</h1>');
+    // return res.end('<h1>Express is up and running for codial!</h1>');
+    return res.render('home',{
+        title:"Home"
+    });
 }
