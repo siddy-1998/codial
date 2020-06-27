3704fc0f5f8b8483478303ef5b9b40deedd72e8b
siddy-1998 <siddhantrathore21@gmail.com>
2020-06-27 15:12:25 +0530

    implement partials

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,6 +1,6 @@
 module.exports.profile = function(req,res){
     // res.end('<h1>User profile</h1>');
     return res.render('user_profile',{
-        title:"Profile"
+        title:"User Profile"
     });
 }
