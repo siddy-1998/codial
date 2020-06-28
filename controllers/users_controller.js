97a24e809d99932d82c8929a71c6bc92fc89c635
siddy-1998 <siddhantrathore21@gmail.com>
2020-06-28 08:30:21 +0530

    rendering and creating form for signIn and signUp pages

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -4,3 +4,30 @@ module.exports.profile = function(req,res){
         title:"User Profile"
     });
 }
+
+//render the sign up page
+module.exports.signUp = function(req,res)
+{
+    return res.render('user_sign_up',{
+        title : "Codeial | SignUp"
+    })
+}
+
+//render the sign in page
+module.exports.signIn = function (req, res) {
+    return res.render('user_sign_in', {
+        title: "Codeial | SignIn"
+    })
+}
+
+//get the sign up data
+module.exports.create = function(req,res)
+{
+    //TODO later
+}
+
+//Sign in and create session for user
+module.exports.createSession = function(req,res)
+{
+    //TODO later
+}
--- routes/users.js
+++ routes/users.js
@@ -5,4 +5,8 @@ const userController = require('../controllers/users_controller');
 
 router.get('/profile',userController.profile);
 
+router.get('/sign-up',userController.signUp);
+
+router.get('/sign-in',userController.signIn);
+
 module.exports = router;
