eccdab43e09c677fd972c7f8248b3ca235bb7a46
siddy-1998 <siddhantrathore21@gmail.com>
2020-07-01 18:41:44 +0530

    set up mongo store for session cookies

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const db = require('./config/mongoose');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal =  require('./config/passport-local-strategy');
+const MongoStore = require('connect-mongo')(session);
 
 app.use(express.urlencoded());
 
@@ -26,6 +27,7 @@ app.set('view engine','ejs');
 app.set('views','./views');
 
 //middleware used to encrypt cookie
+//mongo store is used to store the session cookie in the db
 app.use(session({
     name : 'codeial',
     //TODO : change the secret before deployement in production mode
@@ -34,7 +36,13 @@ app.use(session({
     resave : false,
     cookie : {
         maxAge : (1000*60*100)
-    }
+    },
+    store : new MongoStore({
+        mongooseConnection : db,
+        autoRemove : 'disabled'
+    },function(err){
+        console.log(err || 'connect-mongodb setup ok');
+    })
 }));
 
 app.use(passport.initialize());
