01a70806e17b4549c408fdce39cc893cfcc67251
siddy-1998 <siddhantrathore21@gmail.com>
2020-07-03 19:40:47 +0530

    create schema for comment and add comment to db

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,8 @@ router.use('/users',require('./users'));
 
 router.use('/posts',require('./posts'));
 
+router.use('/comments',require('./comments'));
+
 //for any futher routes, access from here
 //router.use('/routerName',require('./routerFile'))
 
