32fca1eb601869ad01aa3a6e294d26151d958099
siddy-1998 <siddhantrathore21@gmail.com>
2020-07-05 16:03:56 +0530

    update user's profile

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -13,6 +13,19 @@ module.exports.profile = function(req,res){
    
 }
 
+//update name and email
+
+module.exports.update = function(req,res){
+    if(req.user.id == req.params.id){
+
+        User.findByIdAndUpdate(req.params.id,req.body,function(err,user){
+            return res.redirect('back');
+        });
+    }else{
+        return res.status(401).send('Unauthorized');
+    }
+}
+
 //render the sign up page
 module.exports.signUp = function(req,res)
 {  
--- routes/users.js
+++ routes/users.js
@@ -7,6 +7,9 @@ const userController = require('../controllers/users_controller');
 
 router.get('/profile/:id',passport.checkAuthentication ,userController.profile);
 
+router.post('/update/:id', passport.checkAuthentication, userController.update);
+
+
 router.get('/sign-up',userController.signUp);
 
 router.get('/sign-in',userController.signIn);
